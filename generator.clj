6010e9a30e096957a6dd7fe11b1b1b1f6828ca97
Bobby Towers <porkostomus@mail.com>
2023-03-06 04:04:44 -0800

    create example solution

---
+++ generator.clj
@@ -0,0 +1,99 @@
+#!/usr/bin/env bb
+
+(require '[cheshire.core :as json]
+         '[babashka.fs :as fs]
+         '[clojure.string :as str])
+
+(comment 
+  (def slug "yacht")
+  )
+
+(def data
+  (let [url "https://raw.githubusercontent.com/exercism/problem-specifications/main/exercises/"]
+    {:canonical-data (json/parse-string (slurp (str url "/" slug "/canonical-data.json")) true)
+     :description (slurp (str url "/" slug "/description.md"))
+     :metadata (slurp (str url "/" slug "/metadata.toml"))}))
+
+(defn init-deps [data]
+  (fs/create-dirs (fs/path "exercises" "practice"
+                           (:exercise data) "src"))
+  (spit (str (fs/file "exercises" "practice"
+                      (:exercise data)
+                      "deps.edn"))
+        "{:aliases {:test {:extra-paths [\"test\"]
+                 :extra-deps {io.github.cognitect-labs/test-runner 
+                              {:git/url \"https://github.com/cognitect-labs/test-runner.git\"
+                               :sha \"705ad25bbf0228b1c38d0244a36001c2987d7337\"}}
+                 :main-opts [\"-m\" \"cognitect.test-runner\"]
+                 :exec-fn cognitect.test-runner.api/test}}}"))
+
+(defn init-lein [data]
+  (let [slug (:exercise data)]
+    (spit (str (fs/file "exercises" "practice"
+                        (:exercise data) "project.clj"))
+          (str "(defproject " slug " \"0.1.0-SNAPSHOT\"
+  :description \"" slug " exercise.\"
+  :url \"https://github.com/exercism/clojure/tree/master/exercises/" slug "\"
+  :dependencies [[org.clojure/clojure \"1.10.0\"]])
+"))))
+
+(defn test-ns-form [data]
+  (str "(ns " (:exercise data) "-test
+  (:require [clojure.test :refer [deftest testing is]]\n             "
+       (:exercise data) "))\n\n"))
+
+(defn src-ns-form [data]
+  (str "(ns " (:exercise data) ")\n\n"))
+
+(defn testing-form [slug test-case]
+  (let [property (symbol (str slug "/" (:property test-case)))
+        input (:input test-case)
+        args (map #(get input %) (keys input))]
+    (str "  (testing \"" (:description test-case) "\"
+     (is (= " (:expected test-case) " "
+         (reverse (into (list property) args)) ")))")))
+
+(defn testing-forms
+  "Outputs a sequence of the test cases for a given property name
+   given its name as a string and the canonical data."
+  [property data]
+  (let [test-cases (filter #(= property (:property %)) (:cases data))]
+    (map #(testing-form (:exercise data) %) test-cases)))
+
+(defn deftest-forms [data]
+  (for [property (distinct (map :property (:cases data)))]
+    (str "(deftest " property "-test\n"
+         (apply str (interpose "\n"
+                               (testing-forms property data)))
+         ")")))
+
+(defn init-tests! [data]
+  (fs/create-dir (fs/path "exercises" "practice"
+                          (:exercise data) "test"))
+  (spit (fs/file "exercises" "practice"
+                 (:exercise data) "test"
+                 (str (str/replace (:exercise data) "-" "_")
+                      "_test.clj"))
+        (str (test-ns-form data)
+             (apply str (interpose "\n\n"
+                                   (deftest-forms data))))))
+
+(defn init-src! [data]
+  (spit (str (fs/file "exercises" "practice" (:exercise data) "src"
+                      (str (str/replace (:exercise data) "-" "_") ".clj")))
+        (str (src-ns-form data)
+             (apply str (interpose "\n\n"
+                                   (for [property (distinct (map :property (:cases data)))]
+                                     (str "(defn " property " []\n  )")))))))
+
+(defn init-description! [data]
+   (fs/create-dir (fs/path "exercises" "practice"
+                           (:exercise (:canonical-data data)) ".docs"))
+  (spit (str (fs/file "exercises" "practice"
+                      (:exercise (:canonical-data data))
+                      ".docs" "instructions.md")) 
+        (:description data)))
+
+(init-description! data)
+
+(println (str "slug: " (first *command-line-args*)))
